503b19a2ed144c26424c6f0e591ebb1ae7280e99
qiuhai <55704123ggdd@163.com>
2016-11-06 15:39:07 +0800

    1

--- src/aty/log/service/A.java
+++ src/aty/log/service/A.java
@@ -2,9 +2,21 @@ package aty.log.service;
 
 import org.apache.log4j.Logger;
 
-public class A {
+public class A implements Cloneable {
 	 private static Logger logger = Logger.getLogger(A.class);  
 	 public void run() {  
 	        logger.error("log in A.java");  
 	    }  
+	 
+	 public A(){
+		System.out.println("A...");
+	 };
+	 
+	 public static void main(String[] args) throws InstantiationException, IllegalAccessException, ClassNotFoundException, CloneNotSupportedException {
+		A a = (A) Class.forName(A.class.getName()).newInstance();
+		A b =  (A) a.clone();
+		System.out.println(a);
+		System.out.println(b);
+	}
+	 
 }
